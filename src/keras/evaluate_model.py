2b35939c0e890f82d7640e95694d29e3de7446a3
Koen Hufkens <koen.hufkens@gmail.com>
2023-10-11 17:53:02 +0200

    dockerfile instructions + model workflow

--- src/keras/evaluate_model.py
+++ src/keras/evaluate_model.py
@@ -155,177 +155,44 @@ train_ds = prepare_dataset(train_img_paths, train_labels_cleaned)
 validation_ds = prepare_dataset(validation_img_paths, validation_labels_cleaned)
 test_ds = prepare_dataset(test_img_paths, test_labels_cleaned)
 
-for data in validation_ds.take(1):
-    images, labels = data["image"], data["label"]
+# load the model
+prediction_model = tf.keras.models.load_model("opt_model")
 
+# A utility function to decode the output of the network.
+def decode_batch_predictions(pred):
+    input_len = np.ones(pred.shape[0]) * pred.shape[1]
+    # Use greedy search. For complex tasks, you can use beam search.
+    results = keras.backend.ctc_decode(pred, input_length=input_len, greedy=True)[0][0][
+        :, :max_len
+    ]
+    # Iterate over the results and get back the text.
+    output_text = []
+    for res in results:
+        res = tf.gather(res, tf.where(tf.math.not_equal(res, -1)))
+        res = tf.strings.reduce_join(num_to_char(res)).numpy().decode("utf-8")
+        output_text.append(res)
+    return output_text
+
+
+#  Let's check results on some test samples.
+for batch in test_ds.take(1):
+    batch_images = batch["image"]
     _, ax = plt.subplots(4, 4, figsize=(15, 8))
 
+    preds = prediction_model.predict(batch_images)
+    pred_texts = decode_batch_predictions(preds)
+
     for i in range(16):
-        img = images[i]
+        img = batch_images[i]
         img = tf.image.flip_left_right(img)
         img = tf.transpose(img, perm=[1, 0, 2])
         img = (img * 255.0).numpy().clip(0, 255).astype(np.uint8)
         img = img[:, :, 0]
 
-        # Gather indices where label!= padding_token.
-        label = labels[i]
-        indices = tf.gather(label, tf.where(tf.math.not_equal(label, padding_token)))
-        # Convert to string.
-        label = tf.strings.reduce_join(num_to_char(indices))
-        label = label.numpy().decode("utf-8")
-
+        title = f"Prediction: {pred_texts[i]}"
         ax[i // 4, i % 4].imshow(img, cmap="gray")
-        ax[i // 4, i % 4].set_title(label)
+        ax[i // 4, i % 4].set_title(title)
         ax[i // 4, i % 4].axis("off")
 
 plt.show()
 
-class CTCLayer(keras.layers.Layer):
-    def __init__(self, name=None):
-        super().__init__(name=name)
-        self.loss_fn = keras.backend.ctc_batch_cost
-
-    def call(self, y_true, y_pred):
-        batch_len = tf.cast(tf.shape(y_true)[0], dtype="int64")
-        input_length = tf.cast(tf.shape(y_pred)[1], dtype="int64")
-        label_length = tf.cast(tf.shape(y_true)[1], dtype="int64")
-
-        input_length = input_length * tf.ones(shape=(batch_len, 1), dtype="int64")
-        label_length = label_length * tf.ones(shape=(batch_len, 1), dtype="int64")
-        loss = self.loss_fn(y_true, y_pred, input_length, label_length)
-        self.add_loss(loss)
-
-        # At test time, just return the computed predictions.
-        return y_pred
-
-def build_model():
-    # Inputs to the model
-    input_img = keras.Input(shape=(image_width, image_height, 1), name="image")
-    labels = keras.layers.Input(name="label", shape=(None,))
-
-    # First conv block.
-    x = keras.layers.Conv2D(
-        32,
-        (3, 3),
-        activation="relu",
-        kernel_initializer="he_normal",
-        padding="same",
-        name="Conv1",
-    )(input_img)
-    x = keras.layers.MaxPooling2D((2, 2), name="pool1")(x)
-
-    # Second conv block.
-    x = keras.layers.Conv2D(
-        64,
-        (3, 3),
-        activation="relu",
-        kernel_initializer="he_normal",
-        padding="same",
-        name="Conv2",
-    )(x)
-    x = keras.layers.MaxPooling2D((2, 2), name="pool2")(x)
-
-    # We have used two max pool with pool size and strides 2.
-    # Hence, downsampled feature maps are 4x smaller. The number of
-    # filters in the last layer is 64. Reshape accordingly before
-    # passing the output to the RNN part of the model.
-    new_shape = ((image_width // 4), (image_height // 4) * 64)
-    x = keras.layers.Reshape(target_shape=new_shape, name="reshape")(x)
-    x = keras.layers.Dense(64, activation="relu", name="dense1")(x)
-    x = keras.layers.Dropout(0.2)(x)
-
-    # RNNs.
-    x = keras.layers.Bidirectional(
-        keras.layers.LSTM(128, return_sequences=True, dropout=0.25)
-    )(x)
-    x = keras.layers.Bidirectional(
-        keras.layers.LSTM(64, return_sequences=True, dropout=0.25)
-    )(x)
-
-    # +2 is to account for the two special tokens introduced by the CTC loss.
-    # The recommendation comes here: https://git.io/J0eXP.
-    x = keras.layers.Dense(
-        len(char_to_num.get_vocabulary()) + 2, activation="softmax", name="dense2"
-    )(x)
-
-    # Add CTC layer for calculating CTC loss at each step.
-    output = CTCLayer(name="ctc_loss")(labels, x)
-
-    # Define the model.
-    model = keras.models.Model(
-        inputs=[input_img, labels], outputs=output, name="handwriting_recognizer"
-    )
-    # Optimizer.
-    opt = keras.optimizers.Adam()
-    # Compile the model and return.
-    model.compile(optimizer=opt)
-    return model
-
-
-# Get the model.
-model = build_model()
-
-validation_images = []
-validation_labels = []
-
-for batch in validation_ds:
-    validation_images.append(batch["image"])
-    validation_labels.append(batch["label"])
-
-def calculate_edit_distance(labels, predictions):
-    # Get a single batch and convert its labels to sparse tensors.
-    saprse_labels = tf.cast(tf.sparse.from_dense(labels), dtype=tf.int64)
-
-    # Make predictions and convert them to sparse tensors.
-    input_len = np.ones(predictions.shape[0]) * predictions.shape[1]
-    predictions_decoded = keras.backend.ctc_decode(
-        predictions, input_length=input_len, greedy=True
-    )[0][0][:, :max_len]
-    sparse_predictions = tf.cast(
-        tf.sparse.from_dense(predictions_decoded), dtype=tf.int64
-    )
-
-    # Compute individual edit distances and average them out.
-    edit_distances = tf.edit_distance(
-        sparse_predictions, saprse_labels, normalize=False
-    )
-    return tf.reduce_mean(edit_distances)
-
-
-class EditDistanceCallback(keras.callbacks.Callback):
-    def __init__(self, pred_model):
-        super().__init__()
-        self.prediction_model = pred_model
-
-    def on_epoch_end(self, epoch, logs=None):
-        edit_distances = []
-
-        for i in range(len(validation_images)):
-            labels = validation_labels[i]
-            predictions = self.prediction_model.predict(validation_images[i])
-            edit_distances.append(calculate_edit_distance(labels, predictions).numpy())
-
-        print(
-            f"Mean edit distance for epoch {epoch + 1}: {np.mean(edit_distances):.4f}"
-        )
-
-epochs = 100  # To get good results this should be at least 50.
-
-model = build_model()
-prediction_model = keras.models.Model(
-    model.get_layer(name="image").input,
-    model.get_layer(name="dense2").output
-)
-
-edit_distance_callback = EditDistanceCallback(prediction_model)
-
-# Train the model.
-history = model.fit(
-    train_ds,
-    validation_data=validation_ds,
-    epochs=epochs,
-    callbacks=[edit_distance_callback],
-)
-
-# save the model
-prediction_model.save("opt_model")
